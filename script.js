73d114f9f32c4e23f82989932b5792cb09fe214e
Michail-Angeos Kostoglou <mic.kostoglou@gmail.com>
2025-11-28 14:47:22 +0200

    ...

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ gsap.to(".arrow1", {duration: 2, opacity: 1, delay: 1,  ease: "power1.inOut"});
 gsap.to(".sphere2", {duration: 2, opacity: 1, delay: 1.5, ease: "power1.inOut"});
 gsap.to(".arrow2", {duration: 2, opacity: 1, delay: 2, ease: "power1.inOut"});
 gsap.to(".sphere3", {duration: 2, opacity: 1, delay: 2.5, ease: "power1.inOut"});
+
+gsap.to(".sphere4", {duration: 2, opacity: 1, ease: "power1.inOut"});
+gsap.to(".arrow3", {duration: 2, opacity: 1, delay: 1,  ease: "power1.inOut"});
+gsap.to(".sphere5", {duration: 2, opacity: 1, delay: 1.5, ease: "power1.inOut"});
+
+gsap.to(".b1", {duration: 0.5, left: "35%", top: "155%", delay: 2});
+gsap.to(".b2", {duration: 0.5, left: "35%", top: "160%", delay: 2.5});
+gsap.to(".b3", {duration: 0.5, left: "35%", top: "165%", delay: 3});
+gsap.to(".b4", {duration: 0.5, left: "35%", top: "170%", delay: 3.5});
+
+gsap.to(".rectangle1", {duration: 2, left: "15%"});
+gsap.to(".rectangle1 p", {duration: 2, opacity: 1, delay: 3});
+gsap.to(".rectangle1 li", {duration: 2, opacity: 1, delay: 3});
+
+gsap.to(".rectangle2", {duration: 2, right: "15%"});
+gsap.to(".rectangle2 p", {duration: 2, opacity: 1, delay: 3});
+gsap.to(".rectangle2 li", {duration: 2, opacity: 1, delay: 3});
+
