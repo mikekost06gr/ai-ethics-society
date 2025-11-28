c4ad5ba80a804a8c63b02a083ffdba7246bf5192
Michail-Angeos Kostoglou <mic.kostoglou@gmail.com>
2025-11-28 11:55:26 +0200

    ...

---
+++ script.js
@@ -0,0 +1,7 @@
+gsap.to(".title", {duration: 1.8, top: "2%", ease: "bounce"});
+
+gsap.to(".sphere1", {duration: 2, opacity: 1, ease: "power1.inOut"});
+gsap.to(".arrow1", {duration: 2, opacity: 1, delay: 1,  ease: "power1.inOut"});
+gsap.to(".sphere2", {duration: 2, opacity: 1, delay: 1.5, ease: "power1.inOut"});
+gsap.to(".arrow2", {duration: 2, opacity: 1, delay: 2, ease: "power1.inOut"});
+gsap.to(".sphere3", {duration: 2, opacity: 1, delay: 2.5, ease: "power1.inOut"});
