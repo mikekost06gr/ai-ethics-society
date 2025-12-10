84424466c27e2f24a225ad69d81fdb260fc41f6d
Michail-Angeos Kostoglou <mic.kostoglou@gmail.com>
2025-12-10 23:35:04 +0200

    Colour and font changes

--- script.js
+++ script.js
@@ -177,13 +177,6 @@ gsap.to(".p3Deco", {
 });
 }
 
-/*
-gsap.to(".sphere4", {duration: 2, opacity: 1, ease: "power1.inOut"});
-gsap.to(".arrow3", {duration: 2, opacity: 1, delay: 1,  ease: "power1.inOut"});
-gsap.to(".sphere5", {duration: 2, opacity: 1, delay: 1.5, ease: "power1.inOut"});
-*/
-
-
 gsap.to(".b1", {
   scrollTrigger: {
     trigger: ".ph2",
@@ -229,22 +222,6 @@ gsap.to(".b4", {
   left: "35%",
 });
 
-
-
-/*
-gsap.to(".b1", { duration: 0.5, left: "35%", delay: 2 });
-gsap.to(".b2", { duration: 0.5, left: "35%", delay: 2.5 });
-gsap.to(".b3", { duration: 0.5, left: "35%", delay: 3 });
-gsap.to(".b4", { duration: 0.5, left: "35%", delay: 3.5 });
-
-gsap.to(".rectangle1", {duration: 2, left: "15%"});
-gsap.to(".rectangle1 p", {duration: 2, opacity: 1, delay: 3});
-gsap.to(".rectangle1 li", {duration: 2, opacity: 1, delay: 3});
-
-gsap.to(".rectangle2", {duration: 2, right: "15%"});
-gsap.to(".rectangle2 p", {duration: 2, opacity: 1, delay: 3});
-gsap.to(".rectangle2 li", {duration: 2, opacity: 1, delay: 3});
-*/
 
 
 gsap.to(".p4Deco", {
@@ -309,9 +286,79 @@ gsap.to(".title5", {
   left: "20%",
 });
 
+gsap.to(".title6", {
+  scrollTrigger: {
+    trigger: ".ph5",
+    start: "top center",
+    toggleActions: "play none none none",
+  },
+  duration: 2,
+  left: "0%",
+})
+
+gsap.to(".b9", {
+  scrollTrigger: {
+    trigger: ".ph5",
+    start: "top center",
+    toggleActions: "play none none none",
+  },
+  duration: 2,
+  opacity: 1,
+})
+
+gsap.to(".b10", {
+  scrollTrigger: {
+    trigger: ".ph5",
+    start: "top center",
+    toggleActions: "play none none none",
+  },
+  duration: 2,
+  opacity: 1,
+})
+
+gsap.to(".b11", {
+  scrollTrigger: {
+    trigger: ".ph5",
+    start: "top center",
+    toggleActions: "play none none none",
+  },
+  duration: 2,
+  opacity: 1,
+})
+
+gsap.to(".b12", {
+  scrollTrigger: {
+    trigger: ".ph5",
+    start: "top center",
+    toggleActions: "play none none none",
+  },
+  duration: 2,
+  opacity: 1,
+})
+
+gsap.to(".b13", {
+  scrollTrigger: {
+    trigger: ".ph5",
+    start: "top center",
+    toggleActions: "play none none none",
+  },
+  duration: 2,
+  opacity: 1,
+})
+
+gsap.to(".b14", {
+  scrollTrigger: {
+    trigger: ".ph5",
+    start: "top center",
+    toggleActions: "play none none none",
+  },
+  duration: 2,
+  opacity: 1,
+})
+
 gsap.to(".footer", {
   scrollTrigger: {
-    trigger: ".ph4",
+    trigger: ".ph5",
     start: "mid center",
     toggleActions: "play none none none",
   },
@@ -319,25 +366,3 @@ gsap.to(".footer", {
   opacity: 1,
   bottom: "0%",
   });
-/*
-gsap.to(".p4Deco", { duration: 0.5, left: "-15%", delay: 1 });
-
-gsap.to(".arrow4", { duration: 0.5, opacity: 1, delay: 3 });
-gsap.to(".b5", { durationn: 0.5, opacity: 1, delay: 3.5 });
-gsap.to(".arrow4", { duration: 0.5, opacity: 0, delay: 4 });
-
-gsap.to(".arrow5", { duration: 0.5, opacity: 1, delay: 4 });
-gsap.to(".b6", { durationn: 0.5, opacity: 1, delay: 4.5 });
-gsap.to(".arrow5", { duration: 0.5, opacity: 0, delay: 5.5 });
-
-gsap.to(".arrow6", { duration: 0.5, opacity: 1, delay: 5.5 });
-gsap.to(".b7", { durationn: 0.5, opacity: 1, delay: 6 });
-gsap.to(".arrow6", { duration: 0.5, opacity: 0, delay: 7 });
-
-gsap.to(".arrow7", { duration: 0.5, opacity: 1, delay: 7 });
-gsap.to(".b8", { durationn: 0.5, opacity: 1, delay: 7.5 });
-gsap.to(".arrow7", { duration: 0.5, opacity: 0, delay: 8.5 });
-
-gsap.to(".oval2", { duration: 0.5, opacity: 1, delay: 9.5 });
-gsap.to(".oval2 h3", { duration: 0.5, opacity: 1, delay: 9.5 });
-*/
